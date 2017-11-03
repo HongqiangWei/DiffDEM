eb0e51176c1efc95e734902c42ec023f283cf7a9
HongqiangWei <30711558+HongqiangWei@users.noreply.github.com>
2017-11-03 19:35:05 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <queue>
+#include <algorithm>
+#include "dem.h"
+#include "Node.h"
+#include "utils.h"
+#include <time.h>
+#include <list>
+#include <unordered_map>
+
+using namespace std;
+using std::cout;
+using std::endl;
+using std::string;
+using std::getline;
+using std::fstream;
+using std::ifstream;
+using std::priority_queue;
+using std::binary_function;
+
+
+typedef std::vector<Node> NodeVector;
+typedef std::priority_queue<Node, NodeVector, Node::Greater> PriorityQueue;
+
+//compute stats for a DEM
+void calculateStatistics(const CDEM& dem, double* min, double* max, double* mean, double* stdDev)
+{
+	int width = dem.Get_NX();
+	int height = dem.Get_NY();
+
+	int validElements = 0;
+	double minValue, maxValue;
+	double sum = 0.0;
+	double sumSqurVal = 0.0;
+	for (int row = 0; row < height; row++)
+	{
+		for (int col = 0; col < width; col++)
+		{
+			if (!dem.is_NoData(row, col))
+			{
+				double value = dem.asFloat(row, col);
+				
+				if (validElements == 0)
+				{
+					minValue = maxValue = value;
+				}
+				validElements++;
+				if (minValue > value)
+				{
+					minValue = value;
+				}
+				if (maxValue < value)
+				{
+					maxValue = value;
+				}
+
+				sum += value;
+				sumSqurVal += (value * value);
+			}
+		}
+	}
+
+	double meanValue = sum / validElements;
+	double stdDevValue = sqrt((sumSqurVal / validElements) - (meanValue * meanValue));
+	*min = minValue;
+	*max = maxValue;
+	*mean = meanValue;
+	*stdDev = stdDevValue;
+}
+bool Diff(const char *demA, const char *demB, char *resultPath, GDALDataType type, double nodatavalue)
+{
+	CDEM originDEM;
+	double originDEMgeoTransformEles[6];
+	if (!readTIFF(demA, type, originDEM, originDEMgeoTransformEles))
+	{
+		cout<<"Failed to read tiff file!"<<endl;
+		return true;
+	}
+	CDEM DEMarc;
+	double DEMarcgeoTransformEles[6];
+	if (!readTIFF(demB, type, DEMarc, DEMarcgeoTransformEles))
+	{
+		cout<<"Failed to read tiff file!"<<endl;
+		return true;
+	}
+	CDEM* result = diff(originDEM, DEMarc);
+	double min, max, mean, stdDev;
+	calculateStatistics(*result, &min, &max, &mean, &stdDev);
+	if (fabs(min-max)>0.00001f) {
+		printf("\n**********************************************\n*************************************************\nDEMs are different. min=%lf, max=%lf\n",min,max);
+		return false;
+	}
+	else {
+		printf("\nDEMs are identical.\n");
+	}
+	delete result;
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+	
+	//argc=5;
+	//argv[1]="wei-diff";
+	//argv[2]="D:\\测试数据\\DEM Data\\原始文件\\Size5000\\grant-3m_5000_RandomFillwei.tif";
+	//argv[3]="D:\\测试数据\\DEM Data\\原始文件\\Size5000\\grant-3m_5000_RandomFillzhou.tif";
+	//argv[4]="D:\\测试数据\\DEM Data\\原始文件\\Size5000\\grant-3m_5000_RandomFill_testsub.tif";
+	if (argc < 5)
+	{
+		return 1;
+	}
+	string pathsrc1(argv[2]);
+	string pathsrc2(argv[3]);
+	string outputFilledPath(argv[4]);
+	size_t indexsrc1 = pathsrc1.find(".tif");
+	size_t indexsrc2 = pathsrc1.find(".tif");
+	if (indexsrc1==string::npos&&indexsrc2==string::npos) {
+		cout<<"Input file name should have an extension of '.tif'"<<endl;
+		return 1;
+	}
+	char* method=argv[1];
+	string strFolder = pathsrc1.substr(0, indexsrc1);
+
+	if (strcmp(method,"wei-diff")==0)
+	{
+		bool result=Diff(&pathsrc1[0],&pathsrc2[0],&outputFilledPath[0],GDT_Float32,-9999); 
+		if (result)
+		{
+			cout<<"There is no difference between the two images!"<<endl;
+		}
+		else{cout<<"There is difference between the two images!!!!!!!!!!!!!!"<<endl;}
+	}
+	cout<<"------------------------------------------------------------------------------"<<endl;
+	return 0;
+}
+
---
+++ utils.cpp
@@ -0,0 +1,156 @@
+#include "utils.h"
+#include "gdal_priv.h"
+#include "dem.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+//create a new GeoTIFF file
+bool  CreateGeoTIFF(char* path,int height, int width,void* pData, GDALDataType type, double* geoTransformArray6Eles,
+					double* min, double* max, double* mean, double* stdDev, double nodatavalue)
+{
+    GDALDataset *poDataset;   
+    GDALAllRegister();
+	CPLSetConfigOption("GDAL_FILENAME_IS_UTF8","NO");
+ 
+	GDALDriver* poDriver = GetGDALDriverManager()->GetDriverByName("GTiff");
+    char **papszOptions = NULL;
+    poDataset = poDriver->Create(path,width, height, 1, type, 
+                                papszOptions );
+	
+
+	if (geoTransformArray6Eles != NULL)
+		poDataset->SetGeoTransform(geoTransformArray6Eles);
+
+
+	GDALRasterBand* poBand;
+	poBand= poDataset->GetRasterBand(1);
+	
+	poBand->SetNoDataValue(nodatavalue);
+
+	if (min != NULL && max != NULL && mean != NULL && stdDev != NULL)
+	{
+		poBand->SetStatistics(*min, *max, *mean, *stdDev);
+	}
+	poBand->RasterIO( GF_Write, 0, 0, width, height, 
+                      pData, width, height, type, 0, 0 );    
+
+	GDALClose( (GDALDatasetH) poDataset );
+
+	return true;
+}
+//read a DEM GeoTIFF file 
+bool readTIFF(const char* path, GDALDataType type, CDEM& dem, double* geoTransformArray6Eles)
+{
+	GDALDataset *poDataset;   
+    GDALAllRegister();
+	CPLSetConfigOption("GDAL_FILENAME_IS_UTF8","NO");
+	poDataset = (GDALDataset* )GDALOpen(path, GA_ReadOnly);
+	if (poDataset == NULL)
+	{
+		printf("Failed to read the GeoTIFF file\n");
+		return false;
+	}
+
+	GDALRasterBand* poBand;
+	poBand = poDataset->GetRasterBand(1);
+	GDALDataType dataType = poBand->GetRasterDataType();
+	if (dataType != type)
+	{
+		return false;
+	}
+	if (geoTransformArray6Eles == NULL)
+	{
+		printf("Transformation parameters can not be NULL\n");
+		return false;
+	}
+
+	memset(geoTransformArray6Eles, 0, 6);
+	poDataset->GetGeoTransform(geoTransformArray6Eles);
+
+	dem.SetWidth(poBand->GetXSize());
+	dem.SetHeight(poBand->GetYSize());
+	
+	if (!dem.Allocate()) return false;
+
+	poBand->RasterIO(GF_Read, 0, 0, dem.Get_NX(), dem.Get_NY(), 
+		(void *)dem.getDEMdata(), dem.Get_NX(), dem.Get_NY(), dataType, 0, 0);
+
+	GDALClose((GDALDatasetH)poDataset);
+	return true;
+}
+/*
+*	neighbor index
+*	5  6  7
+*	4     0
+*	3  2  1
+*/
+int	ix[8]	= { 0, 1, 1, 1, 0,-1,-1,-1 };
+int	iy[8]	= { 1, 1, 0,-1,-1,-1, 0, 1 };
+
+void setNoData(unsigned char* data, int length, unsigned char noDataValue)
+{
+	if (data == NULL || length == 0)
+	{
+		return;
+	}
+
+	for (int i = 0; i < length; i++)
+	{
+		data[i] = noDataValue;
+	}
+}
+void setNoData(float* data, int length, float noDataValue)
+{
+	for (int i = 0; i < length; i++)
+	{
+		data[i] = noDataValue;
+	}
+}
+
+
+const unsigned char value[8] = {128, 64, 32, 16, 8, 4, 2, 1};
+
+CDEM* diff(CDEM& demA, CDEM& demB)
+{
+	CDEM* pRestultDEM = new CDEM();
+
+	int widthA = demA.Get_NX();
+	int heightA = demA.Get_NY();
+	int widthB = demB.Get_NX();
+	int heightB = demB.Get_NY();
+
+	//widthA== widthB??heightA==heightB??
+	if (widthA != widthB || heightA != heightB)
+	{
+		cout<<"Resolution is different!"<<endl;
+		return NULL;
+	}
+
+	pRestultDEM->SetHeight(heightA);
+	pRestultDEM->SetWidth(widthA);
+	if (!pRestultDEM->Allocate())
+	{
+		cout<<"Failed to allocate memory!"<<endl;
+		return NULL;
+	}
+
+	float valueA, valueB;
+	for (int row = 0; row < heightA; row++)
+	{
+		for (int col = 0; col < widthA; col++)
+		{
+			//保证要比较的点是在DEM中，而且是有效值
+			if (demA.is_InGrid(row, col) && !demA.is_NoData(row, col) 
+				&& demB.is_InGrid(row, col) && !demB.is_NoData(row, col))
+			{
+				valueA = demA.asFloat(row, col);
+				valueB = demB.asFloat(row, col);
+
+				float tmp = valueA - valueB;
+				pRestultDEM->Set_Value(row, col, tmp);
+			}
+		}
+	}
+	return pRestultDEM;
+}
